e62a55e15978267a3881fdaffe3411d07645d299
adrian lemos <alemos@tecnosoftware.com>
2023-06-13 13:26:55 -0300

    diseño de componente card para criptomonedas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CotizacionCardComponent } from './cotizacion-card/cotizacion-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CotizacionCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cotizacion-card/cotizacion-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cotizacion-card',
+  templateUrl: './cotizacion-card.component.html',
+  styleUrls: ['./cotizacion-card.component.css']
+})
+export class CotizacionCardComponent {
+valor: number = 0;
+cryptomoneda: string = "";
+}
